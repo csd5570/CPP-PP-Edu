123dde99c9b730e0c84dd5e1d21f7cb1c7edf5af
csd5570 <161589941+csd5570@users.noreply.github.com>
2024-04-18 21:14:18 +0300

    coding on main (Chapter 12)

--- Resources/Code/Programming-code/Chapter12/chapter.12.3.cpp
+++ Resources/Code/Programming-code/Chapter12/chapter.12.3.cpp
@@ -12,7 +12,7 @@
 
 #include "Simple_window.h"    // get access to our window library
 #include "Graph.h"            // get access to our graphics library facilities
-
+#include "ImageData.h"
 //------------------------------------------------------------------------------
 
 int main()
@@ -20,24 +20,25 @@ int main()
     using namespace Graph_lib;   // our graphics facilities are in Graph_lib
 
     Point tl(100,100);           // to become top left  corner of window
-
-    Simple_window win(tl,600,400,"Canvas");    // make a simple window
-
-    Polygon poly;                // make a shape (a polygon)
-
-    poly.add(Point(300,200));    // add a point
-    poly.add(Point(350,100));    // add another point
-    poly.add(Point(400,200));    // add a third point 
-
-    poly.set_color(Color::red);  // adjust properties of poly
-
-    Text t(Point(150,50), "Hello World of HY150! ");
-    t.set_font(Graph_lib::Font::times_bold);
-    t.set_font_size(20);
-
-    win.attach(t);
-    
-    win.attach (poly);           // connect poly to the window
+    string fname;
+    cin >> fname;
+    ImageData data(fname);
+    Simple_window win(tl, data.getWidth() * 20 + 70, data.getHeight() * 20, fname);    // make a simple window
+    vector<vector<Pixel>> img = data.getImage();
+    int x = 0;
+    int y = 0;
+    for(int j = 0; j < data.getHeight(); j++){
+        y=0;
+        for(int i = 0; i < data.getWidth(); i++){
+            Pixel p = img[j][i];
+            Rectangle r(Point(x,y),20,20);
+            Color c = fl_rgb_color(p.getColor().x(),p.getColor().y(),p.getColor().z());
+            r.set_fill_color(c);
+            y += 20;
+            win.attach(r);
+        }
+        x+=20;
+    }
 
     win.wait_for_button();       // give control to the display engine
 }
